f6afca57b1208d33db4f50c97f164d504ebd758a
Jjaakkoo <jakozeng@gmail.com>
2026-07-23 16:31:38 -0600

    I ts works

--- dt_scheduler-frontend/next.config.ts
+++ dt_scheduler-frontend/next.config.ts
@@ -2,14 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    return process.env.NODE_ENV === "development"
-      ? [
-          {
-            source: "/api/:path*",
-            destination: "http://127.0.0.1:5328/api/:path*",
-          },
-        ]
-      : [];
+    return [
+      {
+        source: "/api/:path*",
+        destination: "/api/main", 
+      },
+    ];
   },
 };
 
